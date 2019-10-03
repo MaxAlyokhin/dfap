f711103b3dd1ebe93bc8fd687820b175d43221e3
Max Alyokhin <alexmaxvik@yandex.ru>
2019-10-03 15:46:12 +0300

    Desktop version is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
 
-  $('.section-header');
-  $('.section-first-section');
-  $('.section-razyob');
-
 // Получение длины SVG стрелки
-
+  setTimeout(function(){
   // Находим ширину блока
   let description_width = $('.description-header-en').css('width');
   console.log(description_width);
@@ -29,7 +25,107 @@ $(document).ready(function() {
 
   let description_width_value = parseInt(description_width.match(/\d+/))
   let arrow_language_width = description_width_value - 67 + 'px'
-  $('.arrow').attr('width', arrow_language_width);
+  $('.arrow-ru, .arrow-en, .arrow-navigation-0, .arrow-navigation-1, .arrow-navigation-2').attr('width', arrow_language_width);
+  }, 100)
+// Переключатель меню
+  // Языки
+  // Скрываем всё на русском
+  $('.arrow-ru').fadeOut(1);
+  $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeOut(1);
+
+  let active_en = 1;
+  $('.menu-language-en').click(function() {
+    if (!active_en) {
+      $('.arrow-ru').fadeOut(3000, 'easeOutQuart');
+      $('.arrow-en').fadeIn(3000, 'easeOutQuart');
+
+      $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeOut(1500, 'easeOutQuart');
+      setTimeout(function(){
+        $('.description-header-en, .description-first-section-en, .description-razyob-en').fadeIn(1500, 'easeOutQuart'); 
+      }, 1500) 
+
+      active_en = 1;
+    }
+  })
+
+  $('.menu-language-ru').click(function() {
+    if (active_en) {
+      $('.arrow-ru').fadeIn(3000, 'easeOutQuart');
+      $('.arrow-en').fadeOut(3000, 'easeOutQuart');
+
+      setTimeout(function(){
+        $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeIn(1500, 'easeOutQuart'); 
+      }, 1500)
+      $('.description-header-en, .description-first-section-en, .description-razyob-en').fadeOut(1500, 'easeOutQuart');  
+
+      active_en = 0;
+    }
+  })
+  // Навигация
+  // Скрываем лишние стрелки
+  $('.arrow-navigation-1').fadeOut(1);
+  $('.arrow-navigation-2').fadeOut(1);
+  // Скрываем лишние секции
+  $('.section-first-section').fadeOut(1);
+  $('.section-razyob').fadeOut(1);
+
+  let active_navigation_0 = 1;
+  let active_navigation_1 = 0;
+  let active_navigation_2 = 0;
+
+  $('.menu-navigation-0-item').click(function() {
+    if (!active_navigation_0) {
+      $('.arrow-navigation-0').fadeIn(1500, 'easeOutQuart');
+      $('.arrow-navigation-1').fadeOut(1500, 'easeOutQuart');
+      $('.arrow-navigation-2').fadeOut(1500, 'easeOutQuart');
+
+      // $('.section-header').fadeIn(1500, 'easeOutQuart');
+      $('.section-first-section').fadeOut(1500, 'easeOutQuart');
+      $('.section-razyob').fadeOut(1500, 'easeOutQuart');
+
+      active_navigation_0 = 1;
+      active_navigation_1 = 0;
+      active_navigation_2 = 0;
+    }
+  })
+
+  $('.menu-navigation-1-item').click(function() {
+    if (!active_navigation_1) {
+      $('.arrow-navigation-0').fadeOut(1500, 'easeOutQuart');
+      $('.arrow-navigation-1').fadeIn(1500, 'easeOutQuart');
+      $('.arrow-navigation-2').fadeOut(1500, 'easeOutQuart');
+
+      // $('.section-header').fadeOut(1500, 'easeOutQuart');
+      $('.section-first-section').fadeIn(1500, 'easeOutQuart');
+      $('.section-razyob').fadeOut(1500, 'easeOutQuart');
+
+      active_navigation_0 = 0;
+      active_navigation_1 = 1;
+      active_navigation_2 = 0;
+    }
+  })
+
+  $('.menu-navigation-2-item').click(function() {
+    if (!active_navigation_2) {
+      $('.arrow-navigation-0').fadeOut(1500, 'easeOutQuart');
+      $('.arrow-navigation-1').fadeOut(1500, 'easeOutQuart');
+      $('.arrow-navigation-2').fadeIn(1500, 'easeOutQuart');
+
+      // $('.section-header').fadeOut(1500, 'easeOutQuart');
+      $('.section-first-section').fadeOut(1500, 'easeOutQuart');
+      $('.section-razyob').fadeIn(1500, 'easeOutQuart');
+
+      active_navigation_0 = 0;
+      active_navigation_1 = 0;
+      active_navigation_2 = 1;
+    }
+  })
+
+  
+
+
+  
+  
 })
 
 // click = 1;
