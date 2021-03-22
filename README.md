93e634acc9a6bbb62451f365901c6c2113d82391
Max Alyokhin <55048182+MaxAlyokhin@users.noreply.github.com>
2021-03-22 23:46:11 +0300

    update 22.03.21

--- README.md
+++ README.md
@@ -1,37 +1,15 @@
-# DFAP
+# Website for DFAP sound-art project
 
-## Website for DFAP sound-art project
- 
- Instruments: HTML5, CSS3, Flexbox, Sass, JS (jQuery), Gulp, VS Code, npm, SVG
- 
- ## Digital fixation of audio processes
- 
+![](https://dfap.stranno.su/img/design.png)
+
+https://dfap.stranno.su
+
+Stack: HTML5, Sass, jQuery, Gulp
+
+## Digital fixation of audio processes
+
 Independent project with a nonbrand but exact name from Krasnodar, Russia, specializing in field recording and musique concrète. All sounds are captured by a digital recorder (which is attached to an extension pole) and composed into tracks.
- 
- ## Project implementation
- 
- ![](https://dfap.stranno.su/img/design.png)
- 
+
+## Project implementation
+
 Own design layout is made in Figma. The site is a set of screens switched by JS using the navigation menu. The layout of all elements of the interface is written using Flexbox, and the main Sass-file with styles is written with a deep nesting, which gives a highly specific CSS after compilation. Separately, the mobile version of the site and the rearrangement of the navigation menu and text blocks with images is completed.
-
-You can check the project by clicking here:
-https://dfap.stranno.su
-
-# DFAP
-
-## Веб-сайт для sound-art проекта DFAP
-
-Инструменты: HTML5, CSS3, Flexbox, Sass, JS (jQuery), Gulp, VS Code, npm, SVG
-
-## Цифровая фиксация аудио процессов
-
-Краснодарский независимый проект с небрендовым, но точным названием, специализирующийся на field recording и musique concrète. Все звуки записываются на рекордер (иногда с помощью палки-удлинителя) и компилируются в треки.
- 
- ## Реализация
-
-Собственный макет дизайна сделан в Figma. Сайт представляет собой набор экранов, переключаемых JS с помощью меню навигации. Расположение всех элементов интерфейса написано с использованием Flexbox, а основной Sass-файл со стилями написан с глубокой вложенностью, дающей высокоспецифичный CSS после компилляции. Отдельно свёрстана мобильная версия сайта с перестроением меню навигации и блоков текста с изображениями.
-
-Проект доступен по ссылке:
-https://dfap.stranno.su
-
- 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,152 +1,153 @@
-$(document).ready(function() {
-
-// Получение длины SVG стрелки
-  setInterval(function(){
+$(document).ready(function () {
+  // Получение длины SVG стрелки
+  setInterval(function () {
     // Находим ширину блока
-    if (active_en == 1) { var description_width = $('.description-header-en').css('width'); }
-    if (active_en == 0) { var description_width = $('.description-header-ru').css('width'); }
+    if (active_en == 1) {
+      var description_width = $('.description-header-en').css('width')
+    }
+    if (active_en == 0) {
+      var description_width = $('.description-header-ru').css('width')
+    }
     // Находим ширину бокового меню
-    let description_width_value = parseInt(description_width.match(/\d+/));
-    let arrow_language_width = description_width_value - 67 + 'px';
-    console.log(arrow_language_width);
+    let description_width_value = parseInt(description_width.match(/\d+/))
+    let arrow_language_width = description_width_value - 67 + 'px'
+    console.log(arrow_language_width)
 
     if (body_width <= 1234) {
-      $('.arrow-ru, .arrow-en, .arrow-navigation-0, .arrow-navigation-1, .arrow-navigation-2').fadeOut(1500, 'easeOutQuart');
-    }
-    else {
-      $('.arrow-ru, .arrow-en, .arrow-navigation-0, .arrow-navigation-1, .arrow-navigation-2').attr('width', arrow_language_width);
+      $('.arrow-ru, .arrow-en, .arrow-navigation-0, .arrow-navigation-1, .arrow-navigation-2').fadeOut(1500, 'easeOutQuart')
+    } else {
+      $('.arrow-ru, .arrow-en, .arrow-navigation-0, .arrow-navigation-1, .arrow-navigation-2').attr('width', arrow_language_width)
     }
   }, 100)
 
-// Переключатель меню
+  // Переключатель меню
   // Языки
   // Скрываем всё на русском
-  $('.arrow-ru').fadeOut(1);
-  $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeOut(1);
+  $('.arrow-ru').fadeOut(1)
+  $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeOut(1)
   // Английский по-умолчанию
-  let active_en = 1;
-  $('.menu-language-en').click(function() {
+  let active_en = 1
+  $('.menu-language-en').click(function () {
     if (!active_en) {
-      $('.arrow-ru').fadeOut(3000, 'easeOutQuart');
-      $('.arrow-en').fadeIn(3000, 'easeOutQuart');
+      $('.arrow-ru').fadeOut(3000, 'easeOutQuart')
+      $('.arrow-en').fadeIn(3000, 'easeOutQuart')
 
-      $('.menu-language-en-item').addClass('underline');
-      $('.menu-language-ru-item').removeClass('underline');
+      $('.menu-language-en-item').addClass('underline')
+      $('.menu-language-ru-item').removeClass('underline')
 
-      $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeOut(1500, 'easeOutQuart');
-      setTimeout(function(){
-        $('.description-header-en, .description-first-section-en, .description-razyob-en').fadeIn(1500, 'easeOutQuart'); 
-        active_en = 1;
-      }, 1500) 
-
-      
+      $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeOut(1500, 'easeOutQuart')
+      setTimeout(function () {
+        $('.description-header-en, .description-first-section-en, .description-razyob-en').fadeIn(1500, 'easeOutQuart')
+        active_en = 1
+      }, 1500)
     }
   })
 
-  $('.menu-language-ru').click(function() {
+  $('.menu-language-ru').click(function () {
     if (active_en) {
-      $('.arrow-ru').fadeIn(3000, 'easeOutQuart');
-      $('.arrow-en').fadeOut(3000, 'easeOutQuart');
-      
-      $('.menu-language-en-item').removeClass('underline');
-      $('.menu-language-ru-item').addClass('underline');
+      $('.arrow-ru').fadeIn(3000, 'easeOutQuart')
+      $('.arrow-en').fadeOut(3000, 'easeOutQuart')
 
-      setTimeout(function(){
-        $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeIn(1500, 'easeOutQuart'); 
-        active_en = 0;
+      $('.menu-language-en-item').removeClass('underline')
+      $('.menu-language-ru-item').addClass('underline')
+
+      setTimeout(function () {
+        $('.description-header-ru, .description-first-section-ru, .description-razyob-ru').fadeIn(1500, 'easeOutQuart')
+        active_en = 0
       }, 1500)
-      $('.description-header-en, .description-first-section-en, .description-razyob-en').fadeOut(1500, 'easeOutQuart');  
-
-      
+      $('.description-header-en, .description-first-section-en, .description-razyob-en').fadeOut(1500, 'easeOutQuart')
     }
   })
   // Навигация
   // Скрываем лишние стрелки
-  $('.arrow-navigation-1').fadeOut(1);
-  $('.arrow-navigation-2').fadeOut(1);
+  $('.arrow-navigation-1').fadeOut(1)
+  $('.arrow-navigation-2').fadeOut(1)
 
   // Скрываем лишние секции
-  $('.section-first-section').fadeOut(1);
-  $('.section-razyob').fadeOut(1);
+  $('.section-first-section').fadeOut(1)
+  $('.section-razyob').fadeOut(1)
 
-  let active_navigation_0 = 1;
-  let active_navigation_1 = 0;
-  let active_navigation_2 = 0;
+  let active_navigation_0 = 1
+  let active_navigation_1 = 0
+  let active_navigation_2 = 0
 
-  let body_width = $('body').css('width');
-  body_width = parseInt(body_width.match(/\d+/));
-  console.log(body_width);
+  let body_width = $('body').css('width')
+  body_width = parseInt(body_width.match(/\d+/))
+  console.log(body_width)
 
-  $('.menu-navigation-0-item').click(function() {
+  $('.menu-navigation-0-item').click(function () {
     if (!active_navigation_0) {
-      $('.arrow-navigation-0').fadeIn(1500, 'easeOutQuart');
-      $('.arrow-navigation-1').fadeOut(1500, 'easeOutQuart');
-      $('.arrow-navigation-2').fadeOut(1500, 'easeOutQuart');
+      $('.arrow-navigation-0').fadeIn(1500, 'easeOutQuart')
+      $('.arrow-navigation-1').fadeOut(1500, 'easeOutQuart')
+      $('.arrow-navigation-2').fadeOut(1500, 'easeOutQuart')
 
-      $('.menu-navigation-0-item').addClass('underline');
-      $('.menu-navigation-1-item').removeClass('underline');
-      $('.menu-navigation-2-item').removeClass('underline');
+      $('.menu-navigation-0-item').addClass('underline')
+      $('.menu-navigation-1-item').removeClass('underline')
+      $('.menu-navigation-2-item').removeClass('underline')
 
-      $('.section-header').fadeIn(1500, 'easeOutQuart');
-      $('.section-first-section').fadeOut(1500, 'easeOutQuart');
-      $('.section-razyob').fadeOut(1500, 'easeOutQuart');
+      $('.section-header').fadeIn(1500, 'easeOutQuart')
+      $('.section-first-section').fadeOut(1500, 'easeOutQuart')
+      $('.section-razyob').fadeOut(1500, 'easeOutQuart')
 
-      active_navigation_0 = 1;
-      active_navigation_1 = 0;
-      active_navigation_2 = 0;
+      active_navigation_0 = 1
+      active_navigation_1 = 0
+      active_navigation_2 = 0
     }
   })
 
-  $('.menu-navigation-1-item').click(function() {
+  $('.menu-navigation-1-item').click(function () {
     if (!active_navigation_1) {
-      $('.arrow-navigation-0').fadeOut(1500, 'easeOutQuart');
-      $('.arrow-navigation-1').fadeIn(1500, 'easeOutQuart');
-      $('.arrow-navigation-2').fadeOut(1500, 'easeOutQuart');
+      $('.arrow-navigation-0').fadeOut(1500, 'easeOutQuart')
+      $('.arrow-navigation-1').fadeIn(1500, 'easeOutQuart')
+      $('.arrow-navigation-2').fadeOut(1500, 'easeOutQuart')
 
-      $('.menu-navigation-0-item').removeClass('underline');
-      $('.menu-navigation-1-item').addClass('underline');
-      $('.menu-navigation-2-item').removeClass('underline');
+      $('.menu-navigation-0-item').removeClass('underline')
+      $('.menu-navigation-1-item').addClass('underline')
+      $('.menu-navigation-2-item').removeClass('underline')
 
       if (body_width <= 1234) {
-        $('.section-header').fadeOut(1500, 'easeOutQuart');
+        $('.section-header').fadeOut(1500, 'easeOutQuart')
       }
-      $('.section-first-section').fadeIn(1500, 'easeOutQuart');
-      $('.section-razyob').fadeOut(1500, 'easeOutQuart');
+      $('.section-first-section').fadeIn(1500, 'easeOutQuart')
+      $('.section-razyob').fadeOut(1500, 'easeOutQuart')
 
-      active_navigation_0 = 0;
-      active_navigation_1 = 1;
-      active_navigation_2 = 0;
+      active_navigation_0 = 0
+      active_navigation_1 = 1
+      active_navigation_2 = 0
     }
   })
 
-  $('.menu-navigation-2-item').click(function() {
+  $('.menu-navigation-2-item').click(function () {
     if (!active_navigation_2) {
-      $('.arrow-navigation-0').fadeOut(1500, 'easeOutQuart');
-      $('.arrow-navigation-1').fadeOut(1500, 'easeOutQuart');
-      $('.arrow-navigation-2').fadeIn(1500, 'easeOutQuart');
+      $('.arrow-navigation-0').fadeOut(1500, 'easeOutQuart')
+      $('.arrow-navigation-1').fadeOut(1500, 'easeOutQuart')
+      $('.arrow-navigation-2').fadeIn(1500, 'easeOutQuart')
 
-      $('.menu-navigation-0-item').removeClass('underline');
-      $('.menu-navigation-1-item').removeClass('underline');
-      $('.menu-navigation-2-item').addClass('underline');
+      $('.menu-navigation-0-item').removeClass('underline')
+      $('.menu-navigation-1-item').removeClass('underline')
+      $('.menu-navigation-2-item').addClass('underline')
 
       if (body_width <= 1234) {
-        $('.section-header').fadeOut(1500, 'easeOutQuart');
+        $('.section-header').fadeOut(1500, 'easeOutQuart')
       }
-      $('.section-first-section').fadeOut(1500, 'easeOutQuart');
-      $('.section-razyob').fadeIn(1500, 'easeOutQuart');
+      $('.section-first-section').fadeOut(1500, 'easeOutQuart')
+      $('.section-razyob').fadeIn(1500, 'easeOutQuart')
 
-      active_navigation_0 = 0;
-      active_navigation_1 = 0;
-      active_navigation_2 = 1;
+      active_navigation_0 = 0
+      active_navigation_1 = 0
+      active_navigation_2 = 1
     }
   })
 
   // Сброс скролла
-  $('.menu').click(function () { 
-    $("html, body").animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutCubic');
-    return false;
-  }); 
-  
+  $('.menu').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      'easeInOutCubic'
+    )
+    return false
+  })
 })
